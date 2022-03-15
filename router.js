5216f636423e17c13c70edf325083c33f5b68130
Luka <kazicluka4@gmail.com>
2022-03-15 15:27:26 +0100

    Added alert message for wrong email or password

--- router.js
+++ router.js
@@ -16,45 +16,47 @@ router.post('/register', async (req, res) => {
        res.render('invalidUser');
     }
      console.log(users);
-})
+});
 
 router.get('/login',  (req, res) => {
-    res.render('base');
+    res.render('login');
     
-})
-router.post('/login', async(req, res, next) => {
+});
+router.post('/login', async(req, res) => {
     const { password, email } = req.body;
-    const isMatch = await bcrypt.compare(password, users[0].password);
-    if(isMatch && email === users[0].email){
+    const doesPasswordsMatch= await bcrypt.compare(password, users[0].password);
+    if(doesPasswordsMatch && email === users[0].email){
         req.session.user = email;
         res.redirect('/route/dashboard');  
     } 
     else {
-        res.redirect('/route/login');
+        setTimeout(() => {
+        res.render('login', {invalidUser: "Wrong email or password"});
+    }, 1000);
     }
-})
+});
 
 
 router.get('/dashboard', (req, res) => {
-if(req.session.user){
+  if(req.session.user){
     res.render('dashboard', {user: req.session.user})
     console.log(req.sessionID);
-}else{
+  }
+  else{
     res.redirect('/');
-}
+  }
 
-})
-
+});
 router.get('/logout', (req, res) => {
     req.session.destroy(function(err){
         if(err){
-          res.send("Error")
+          res.send("Couldnt log out");
         }
         else{
             res.clearCookie('connect.sid');
-            res.render('register', {logout: "Logged Out Successfully!"})
+            res.render('register', {logout: "Logged Out Successfully"});
         }
-    })
-})
+    });
+});
 
 module.exports = router;
